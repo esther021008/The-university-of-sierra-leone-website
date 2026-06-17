27115b7e0a6ffd66e03fa25024647fa08e111f10
esther021008 <kargboesther77@gmail.com>
2026-06-17 10:33:11 +0000

    new code

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector(".menu");
+let navLink = document.querySelector(".list");
+let exist = document.querySelector(".exist");
+
+menu.addEventListener("click", () => {
+
+    navLink.classList.toggle('disappear')
+
+});
